a42259ec42fa48d7ba68d0982cc2c24783e9fded
user <mariiaantoniv@gmail.com>
2025-03-31 11:38:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/* const listItems = document.querySelectorAll('ul.tree li > ul > li');
-// знаходимо всі елементи li всередині списків
-
-listItems.forEach((item) => {
-  const text = item.firstChild.data.trim();
-
-  const span = document.createElement('span');
-
-  span.textContent = text;
-  item.firstChild.data = '';
-  item.insertBefore(span, item.firstChild);
-}); */
-
 const tree = document.querySelector('.tree');
 
 for (const li of tree.querySelectorAll('li')) {
